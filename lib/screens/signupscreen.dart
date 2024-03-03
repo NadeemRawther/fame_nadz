6cb26d3da86c1f960e94a651bd0370df10d030fd
nadeem <nadeemsani786@hotmail.com>
2024-03-03 22:41:03 +0530

    second commit after signup screen

--- lib/screens/signupscreen.dart
+++ lib/screens/signupscreen.dart
@@ -9,6 +9,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:image_picker/image_picker.dart';
+import '../responsive/mobile_screen_layout.dart';
+import '../responsive/responsive_layout_screen.dart';
+import '../responsive/web_screen_layout.dart';
 import '../utils/colors.dart';
 import '../utils/utils.dart';
 
@@ -24,6 +27,7 @@ class _SignupScreenState extends State<SignupScreen> {
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _bioController = TextEditingController();
   final TextEditingController _userNameController = TextEditingController();
+  bool _isLoading = false;
    Uint8List? _img;
 
   @override
@@ -34,6 +38,48 @@ class _SignupScreenState extends State<SignupScreen> {
     _bioController.dispose();
     _userNameController.dispose();
   }
+
+  void signUpUser() async {
+    // set loading to true
+    setState(() {
+      _isLoading = true;
+    });
+
+    // signup user using our authmethodds
+    String res = await AuthMethods().signUpUser(
+        email: _emailController.text,
+        password: _passwordController.text,
+        username: _userNameController.text,
+        bio: _bioController.text,
+        file: _img!);
+    // if string returned is sucess, user has been created
+    if (res == "success") {
+      setState(() {
+        _isLoading = false;
+      });
+      // navigate to the home screen
+      if (context.mounted) {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (context) => const ResponsiveLayout(
+              mobileScreenLayout: MobileScreenLayout(),
+              webScreenLayout: WebScreenLayout(),
+            ),
+          ),
+        );
+      }
+    } else {
+      setState(() {
+        _isLoading = false;
+      });
+      // show the error
+      if (context.mounted) {
+        showSnackBar(context, res);
+      }
+    }
+  }
+
+
   selectImage()async{
     Uint8List im = await pickImage(ImageSource.gallery);
     setState(() {
@@ -51,7 +97,7 @@ class _SignupScreenState extends State<SignupScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Flexible(child: Container(), flex: 1),
+              Flexible(child: Container(), flex: 2),
               SvgPicture.asset(
                 'assets/famenadzlog.svg',
                 color: primaryColor,
@@ -70,7 +116,7 @@ class _SignupScreenState extends State<SignupScreen> {
                   const CircleAvatar(
                     radius: 64,
                     backgroundImage: NetworkImage(
-                        "https://unsplash.com/photos/green-leafed-tree-surrounded-by-fog-during-daytime-S297j2CsdlM"),
+                        "https://media.istockphoto.com/id/1494104649/photo/ai-chatbot-artificial-intelligence-digital-concept.jpg?s=1024x1024&w=is&k=20&c=FQZBVxbXjhgPzm-W-5Jo3O7x7OXLPugwXzTKEu6nV3w="),
                   ),
                   Positioned(
                     bottom: -10,
@@ -116,32 +162,30 @@ class _SignupScreenState extends State<SignupScreen> {
                 height: 24,
               ),
               InkWell(
-                onTap: ()async {
-                  String res = await AuthMethods().signUpUser(
-                    email: _emailController.text,
-                    password: _passwordController.text,
-                    username: _userNameController.text,
-                    bio:_bioController.text,
-                    file: _img!
-
-                  );
-                  print(res);
-                },
+                onTap: signUpUser,
                 child: Container(
                   width: double.infinity,
                   alignment: Alignment.center,
                   padding: const EdgeInsets.symmetric(vertical: 12),
                   decoration: const ShapeDecoration(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(4))),
-                      color: blueColor),
-                  child: const Text("Sign Up"),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(4)),
+                    ),
+                    color: blueColor,
+                  ),
+                  child: !_isLoading
+                      ? const Text(
+                    'Sign up',
+                  )
+                      : const CircularProgressIndicator(
+                    color: primaryColor,
+                  ),
                 ),
               ),
               const SizedBox(
-                height: 24,
+                height: 12,
               ),
-              Flexible(child: Container(), flex: 1),
+              Flexible(child: Container(), flex: 2),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -167,4 +211,6 @@ class _SignupScreenState extends State<SignupScreen> {
       ),
     );
   }
+
+
 }
